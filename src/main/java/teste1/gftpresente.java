c087930879161861056c526845b2f6f9e0e5c47e
kauanmaldonado <kauan.maldonado16@gmail.com>
2021-08-19 17:58:39 -0300

    Initial commit

---
+++ src/main/java/teste1/gftpresente.java
@@ -0,0 +1,34 @@
+package teste1;
+
+public class gftpresente {
+
+    Scanner input = new Scanner(System.in); {
+
+        System.out.println("Digite o seu idioma: ");
+        String tempo = input.next();
+
+        switch (tempo) {
+
+            case "1 ANO":
+                System.out.println("Vale Presente");
+                break;
+            case "2 ANOS":
+                System.out.println("Smart Watch");
+                break;
+            case "5 ANOS":
+                System.out.println("Fone Bluetooth ");
+                break;
+            case "10 ANOS":
+                System.out.println("Vale Jantar");
+                break;
+            case "15 ANOS":
+                System.out.println("Vale Jantar Top");
+                break;
+
+            default:
+                System.out.println("Esse ano não vai ter presente");
+
+        }
+    }
+}
+}
